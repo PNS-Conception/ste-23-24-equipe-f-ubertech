1d0b454f3e58b63136f9017ba455f2f066a5528a
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2023-11-08 17:25:44 +0100

    some fix for add product userStorie

--- src/test/java/tests/AddProduct.java
+++ src/test/java/tests/AddProduct.java
@@ -1,5 +1,4 @@
 package tests;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,21 +11,25 @@ import java.nio.file.ReadOnlyFileSystemException;
 import java.util.ArrayList;
 import java.util.Date;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AddProduct {
     private Product product;
     private Customer customer;
+    private Restaurant restaurant;
 
     @Given("a customer")
     public void a_customer() {
         customer = new Customer("Beurel","Simon");
+        restaurant = new Restaurant("test restaurant", "test address", null);
+        product = new Product(restaurant, "test burger", 7);
     }
-    @When("I add a product to my pendingOrder")
+    @When("they add a product to their pendingOrder")
     public void add_product_to_my_order() {
         customer.addProductToPendingOrder(product);
     }
-    @Then("the product is stored in my pendingOrder")
+    @Then("the product is stored in their pendingOrder")
     public void product_is_correctly_stored() {
         assertTrue(customer.getSizePendingOrder()==1);
     }
