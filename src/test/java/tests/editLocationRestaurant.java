a34d5a08a0554ae6c9a22c2c1014f3f7eb99eb85
TarikAZIKI <tarikaziki1@gmail.com>
2023-11-20 17:34:57 +0100

    Correction d'erreurs

--- src/test/java/tests/editLocationRestaurant.java
+++ src/test/java/tests/editLocationRestaurant.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import sophiatech.Restaurant;
-import sophiatech.System;
 
 import static org.junit.Assert.assertEquals;
 public class editLocationRestaurant {
