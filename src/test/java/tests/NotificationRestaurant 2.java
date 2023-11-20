beb0ddf483a0bdfbbb38baabbf42986069e9357d
TarikAZIKI <tarikaziki1@gmail.com>
2023-11-20 17:34:29 +0100

    Correction d'erreurs

---
+++ src/test/java/tests/NotificationRestaurant 2.java
@@ -0,0 +1,37 @@
+package tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+import sophiatech.*;
+import sophiatech.System;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class NotificationRestaurant {
+    private System system = new System();
+    private Restaurant restaurant;
+    private Customer customer;
+    @Given("a restaurant named {string} and a customer")
+    public void aRestaurantNamedAndACustomer(String arg0) {
+        restaurant = new Restaurant(arg0, "75 rue evariste galois", null);
+        customer = new Customer("Quentin", "Maurois", system,UserType.STUDENT);
+        system.addRestaurant(restaurant);
+    }
+
+    @When("the customer makes a new order for {string}")
+    public void theCustomerMakeANewOrderFor(String arg0) {
+        Product product = new Product(restaurant, "Burger", 15);
+        customer.addProductToPendingOrder(product);
+        customer.payForOrder();
+    }
+
+    @Then("the restaurant receive a notification")
+    public void theRestaurantReceiveANotification() {
+        assertEquals(restaurant.getActiveOrders().size(),1);
+    }
+}
